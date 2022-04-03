cc4ae4184b54ef65652c51245b76875343399025
SHIVA KASHYAP <93374924+Shiva-fighter@users.noreply.github.com>
2022-04-03 14:42:19 -0400

    Add files via upload
    create login with auth

---
+++ context-api/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { LoginContextProvider } from './Contexts/LoginContext'
+
+ReactDOM.render(
+  <React.StrictMode>
+        <LoginContextProvider>
+        <App />
+        </LoginContextProvider>
+
+  </React.StrictMode>,
+  document.getElementById('root')
+)
