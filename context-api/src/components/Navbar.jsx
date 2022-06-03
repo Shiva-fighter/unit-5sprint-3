d643d006f188af035ab7446decac78fa237e85ba
SHIVA KASHYAP <93374924+Shiva-fighter@users.noreply.github.com>
2022-06-03 13:06:55 +0530

    update

--- context-api/src/components/Navbar.jsx
+++ context-api/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { LoginContext } from "../Contexts/LoginContext"
 function Navbar() {
     const {email ,setEmail,password,setPassword,toggleAuth,isAuth, setIsAuth}=useContext(LoginContext)
